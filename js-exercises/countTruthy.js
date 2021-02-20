1e6019516d3ea70f50a54ff6a50c63a7fd70cdf0
Madhushri Patil <madpatil@paypal.com>
2021-02-20 17:12:10 -0500

    count truthy values

---
+++ js-exercises/countTruthy.js
@@ -0,0 +1,21 @@
+function countTruthy(values) {
+    const falsyValues = [0, false, '', null, undefined, NaN];
+    let cnt = 0;
+    for(let i=0; i < values.length; i++) {
+        if (!falsyValues.includes(values[i]))
+            cnt++;
+    }
+    return cnt;
+}
+
+let values = [true, undefined, 1, 'hello', 'js', 80, null];
+let count = countTruthy(values);
+console.log(values, count);
+
+values = [0, 1, 'computer', 80, null, 'hi', false, NaN];
+count = countTruthy(values);
+console.log(values, count);
+
+values = [1, 2, 'computer'];
+count = countTruthy(values);
+console.log(values, count);
